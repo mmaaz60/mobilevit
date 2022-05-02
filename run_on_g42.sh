76acaf8883cf98d0beac86195f5aa3f3c4c57553
Muhammad Maaz <mmaaz60@gmail.com>
2022-05-02 17:40:10 +0400

    Minor fix

--- run_on_g42.sh
+++ run_on_g42.sh
@@ -9,4 +9,4 @@
 ##SBATCH --gpus=16
 
 
-srun python main_train.py --common.config-file config/classification/convnext.yaml --common.results-loc results/convnext --dataset.root-train /nfs/projects/mbzuai/salman/imagenet_1k/train --dataset.root-val /nfs/projects/mbzuai/salman/imagenet_1k/val --dataset.workers 6
+srun python main_train.py --common.config-file config/classification/convnext.yaml --common.results-loc results/convnext --dataset.root-train /nfs/projects/mbzuai/salman/imagenet_1k/train --dataset.root-val /nfs/projects/mbzuai/salman/imagenet_1k/val --dataset.workers 6 --dataset.train-batch-size0 64

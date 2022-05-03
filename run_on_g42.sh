8c2044c437b3977726325356ba18bbbe25a0765e
Muhammad Maaz <mmaaz60@gmail.com>
2022-05-03 04:09:03 +0400

    Updates run_on_g42.sh

--- run_on_g42.sh
+++ run_on_g42.sh
@@ -9,4 +9,6 @@
 ##SBATCH --gpus=16
 
 
-srun python main_train.py --common.config-file config/classification/convnext.yaml --common.results-loc results/convnext --dataset.root-train /nfs/projects/mbzuai/salman/imagenet_1k/train --dataset.root-val /nfs/projects/mbzuai/salman/imagenet_1k/val --dataset.workers 6 --dataset.train-batch-size0 64
+srun python main_train.py --common.config-file "$1" --common.results-loc "$2" --dataset.root-train /nfs/projects/mbzuai/salman/imagenet_1k/train --dataset.root-val /nfs/projects/mbzuai/salman/imagenet_1k/val --dataset.workers 6 --dataset.train-batch-size0 64
+
+# Example run -> sbatch run_on_g42.sh <path to .yaml config> <path to the output dir>

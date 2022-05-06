edef7cec1d22d7e886680c8cf3e04db6295b4600
Muhammad Maaz <mmaaz60@gmail.com>
2022-05-06 07:23:38 +0400

    Adds scripts to run training on mahti.

--- run_on_mahti.sh
+++ run_on_mahti.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
-#SBATCH --job-name=ConvNeXt
+#SBATCH --job-name=coco_mobilenext
 #SBATCH --account=project_2000255
 #SBATCH --partition=gpumedium
 #SBATCH --time=36:00:00
 #SBATCH --nodes=2
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=32
-#SBATCH --gres=gpu:a100:4,nvme:400
+#SBATCH --gres=gpu:a100:4,nvme:200
 ##SBATCH --mem-per-cpu=8000
 
 
-srun --ntasks=2 --ntasks-per-node=1 \
-    tar xf /scratch/project_2001284/anwer/mvit_pretraining/data/train.tar.gz -C "$LOCAL_SCRATCH" --overwrite
-
-srun --ntasks=2 --ntasks-per-node=1 \
-    tar xf /scratch/project_2001284/anwer/mvit_pretraining/data/val.tar.gz -C "$LOCAL_SCRATCH" --overwrite
-
-PYTHONWARNINGS="ignore::Warning" timeout 35.5h srun python main_train.py --common.config-file config/classification/convnext.yaml --common.results-loc results/convnext --dataset.root-train "$LOCAL_SCRATCH/train" --dataset.root-val "$LOCAL_SCRATCH/val" --dataset.workers 10
+timeout 35.5h cvnets-train --common.config-file config/detection/ssd_mobilenext_320.yaml --common.results-loc results_coco_mobilenext
 if [[ $? -eq 124 ]]; then
   sbatch ./run_on_mahti.sh
 fi

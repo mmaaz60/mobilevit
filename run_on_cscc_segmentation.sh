1d7f27a38e6a8bf654b8b34e300e481347456251
Muhammad Maaz <mmaaz60@gmail.com>
2022-05-08 22:21:01 +0400

    Updates slurm script

--- run_on_cscc_segmentation.sh
+++ run_on_cscc_segmentation.sh
@@ -9,4 +9,4 @@
 
 LOCAL_SCRATCH='/home/muhammad.maaz/mobilevit/dataset'
 
-cvnets-train --common.config-file config/segmentation/deeplabv3_mobilenext.yaml --common.results-loc results_deeplabv3_mobilenext --dataset.train-batch-size0 64 --dataset.val-batch-size0 32 --dataset.root-train "$LOCAL_SCRATCH/pascal_voc/pascal_voc/VOCdevkit" --dataset.root-val "$LOCAL_SCRATCH/pascal_voc/pascal_voc/VOCdevkit" --dataset.pascal.coco-root-dir "$LOCAL_SCRATCH/pascal_voc/coco_preprocess"
+python main_train.py --common.config-file config/segmentation/deeplabv3_mobilenext.yaml --common.results-loc results_deeplabv3_mobilenext --dataset.train-batch-size0 64 --dataset.val-batch-size0 32 --dataset.root-train "$LOCAL_SCRATCH/pascal_voc/pascal_voc/VOCdevkit" --dataset.root-val "$LOCAL_SCRATCH/pascal_voc/pascal_voc/VOCdevkit" --dataset.pascal.coco-root-dir "$LOCAL_SCRATCH/pascal_voc/coco_preprocess"
